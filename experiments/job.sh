8255a257b58562370a4b69546ee107ac02da0984
Brunno Goldstein <daewoo4@gmail.com>
2019-02-08 11:40:58 -0200

    Fixed errors into vlab and job scripts

--- experiments/job.sh
+++ experiments/job.sh
@@ -3,15 +3,22 @@
 EXP_PATH=$1
 EXP_RUN=$2
 PROJECT=$3
-FLAGS=$4
-DATASET_PATH=$5
+DATASET_PATH=$4
+FLAGS=$5
 
-export PATH=$PATH:/opt/pbs/default/bin
-export PYTHONPATH=$PYTHONPATH:${HOME}${PROJECT}
+FLAGS="-a resnet50 -e --pretrained --injection --layer=0 -b 256"
 
 . /export/software/anaconda2/etc/profile.d/conda.sh
 conda activate ${PROJECT}
 
+export PATH=$PATH:/opt/pbs/default/bin
+export PYTHONPATH=$PYTHONPATH:${HOME}/${PROJECT}
+
+echo ${PYTHONPATH}
+
+echo "cd ${EXP_PATH}"
 cd ${EXP_PATH}
 
-python ${EXP_RUN} ${FLAGS} ${DATASET_PATH}
+echo "python3 ${EXP_RUN} ${FLAGS} ${DATASET_PATH}"
+
+python3 ${EXP_RUN} ${FLAGS} ${DATASET_PATH}
--- experiments/vlab.sh
+++ experiments/vlab.sh
@@ -8,7 +8,7 @@ PROJECT_PATH=$HOME/$PROJECT
 DATASET_PATH=$HOME/dataset/imagenet
 JOB_PATH=${PROJECT_PATH}/experiments/job.sh
 
-FLAGS=-a resnet50 -e --pretrained --injection --layer=0 -b 256 ${DATASET_PATH}
+FLAGS="-a resnet50 -e --pretrained --injection --layer=0 -b 256"
 
 EXP_PATH=${PROJECT_PATH}/examples/imagenet
 EXP_RUN=resnet_eval.py
@@ -27,7 +27,7 @@ echo "Creating job ${EXP_RUN} with flags ${FLAGS}"
 
 echo 'Submiting job... '
 
-echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH}/${EXP_RUN} ${PROJECT} ${FLAGS} ${DATASET_PATH}"
-qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${FLAGS} ${DATASET_PATH}
+echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS}"
+qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS} 
 
 echo "Job submitted."
