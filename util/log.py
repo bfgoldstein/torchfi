c412a9483d80ed66a46d0405ab680d04009853d0
Brunno Goldstein <daewoo4@gmail.com>
2019-02-27 08:24:39 -0300

    Add Record class to store vital information about FI runs.

--- util/log.py
+++ util/log.py
@@ -57,3 +57,93 @@ def logTestPass():
 
 def logTestError():
     print(bcolors.FAIL + "    error!" + bcolors.ENDC)
+
+
+
+
+class Record(object):
+
+    def __init__(self, model, batch_size, fiLayer, fiFeatures, fiWeights, quant_bfeats, quant_bwts, quant_baccum, 
+                injection=None, quantization=None):
+        """Record constructor
+            
+        Args:
+            model (String): Name of the model in torchvision
+            
+            fiFeatures (bool): If True, faults can be applied on input features
+
+            fiWeights (bool): If True, faults can be applied on weights
+
+                fiFeatures and fiWeights == False: Faults are applied randomly on input features and weighs
+            
+            fiFeatures (1d ndarray): array of integers with bit flipped position of each batch
+            
+            fiBit (1d ndarray): array of integers with bit flipped position of each batch
+            
+            originalValue (1d ndarray): array of floats with value before injection of each batch
+            
+            fiValue (1d ndarray): array of floats with value after injection of each batch
+            
+            fiLocation (tuple of ints): typle indicating location of faul injection
+                e.g.: (filter, row, column)
+
+            quantization (tuple with # bits for (feat, wts, acc)): If not None, quantization 
+                was applied with #bits for features, weights and accumulator
+
+            quant_bfeats (integer): number of bits for input features during quantization
+            
+            quant_bwts (integer): number of bits for weights during quantization
+            
+            quant_baccum (integer): number of bits for accumulators during quantization
+            
+            scores (2d tensor): array of scores (only top 10 values) of each batch
+        """
+
+        self.model = model
+        self.batch_size = batch_size
+
+        self.injection = injection
+        self.fiLayer = fiLayer
+        self.fiFeatures = fiFeatures
+        self.fiWeights = fiWeights
+        self.fiBit = []
+        self.originalValue = []
+        self.fiValue = []
+        self.fiLocation = [] 
+
+        self.quantization = quantization
+        self.quant_bfeats = quant_bfeats
+        self.quant_bwts = quant_bwts
+        self.quant_baccum = quant_baccum
+
+        self.scores = []
+        self.predictions = []
+        self.targets = []
+        self.acc1 = 0
+        self.acc5 = 0
+
+    def addScores(self, tensors):
+        self.scores.append(tensors.cpu())
+    
+    def addPredictions(self, tensors):
+        self.predictions.append(tensors.cpu())
+
+    def addTargets(self, tuples):
+        for tup in tuples:
+            self.targets.append(tup)
+
+    def addFiBit(self, bit):
+        self.fiBit.append(bit)
+
+    def addOriginalValue(self, value):
+        self.originalValue.append(value)
+
+    def addFiValue(self, value):
+        self.fiValue.append(value)
+
+    def addFiLocation(self, loc):
+        self.fiLocation.append(loc)
+
+    def setAccuracies(self, acc1, acc5):
+        self.acc1 = acc1
+        self.acc5 = acc5
