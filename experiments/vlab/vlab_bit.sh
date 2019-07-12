a734f3d81773387e10bf073ece0176c18719ba63
Brunno Goldstein <daewoo4@gmail.com>
2019-07-12 11:40:55 -0300

    Created vlab_bit.sh to launch experiments varying FI bit position; Minor change into vlab.sh

--- experiments/vlab/vlab_bit.sh
+++ experiments/vlab/vlab_bit.sh
@@ -11,9 +11,6 @@ JOB_PATH=${PROJECT_PATH}/experiments/vlab/job.sh
 EXP_PATH=${PROJECT_PATH}/examples/imagenet
 EXP_RUN=resnet_eval.py
 
-# Get command line args
-pruned=$1
-
 # Set job parameters
 queue=skl
 nodes=1
@@ -22,25 +19,11 @@ cpus=8
 # Experiments configurations
 model=resnet50
 niters=5
-nlayers=53
-bit="random"
+layer=0
+nbits=31
 location="weights"
-pruned_prefix=""
 
 FLAGS_BASE="-a ${model} -e --pretrained --batch-size=1"
-
-if [ ${pruned} -eq 1 ]; then
-    pruned_path=$2
-    prune_percent=$3
-    FLAGS_BASE+=" --pruned --pruned_file=${pruned_path}/resnet50_pruned_${prune_percent}_best.pth.tar"
-    prune_compensate=$4
-    if [ ${prune_compensate} -eq 1 ]; then
-        FLAGS_BASE+=" --prune_compensate"
-        pruned_prefix="_pruned_${prune_percent}"
-    elif [ ${prune_compensate} -eq 0 ]; then
-        pruned_prefix="_pruned_${prune_percent}_nocomp"
-    fi
-fi
 
 declare -a dtypes=("fp32" "int16" "int8")
 
@@ -55,19 +38,21 @@ do
  
     if [ ${dtype} == "int16" ]; then
         FLAGS+=" --quantize --quant-feats=16 --quant-wts=16 --quant-accum=64"
+        nbits=15
     elif [ ${dtype} == "int8" ]; then
         FLAGS+=" --quantize --quant-feats=8 --quant-wts=8 --quant-accum=32"
+        nbits=7
     fi
 
-    outputPath=${HOME}/experiments/iccd
+    outputPath=${HOME}/experiments/iccd/bit
     outputFilePrefix=${outputPath}/${dtype}/${model}
-    fidPrefix=${outputFilePrefix}_${dtype}_norm${pruned_prefix}
+    fidPrefix=${outputFilePrefix}_${dtype}_norm
     experiment_out_path=${outputPath}/${dtype}/logs
     
     experiment_config="golden"
     FLAGS_GOLDEN=${FLAGS}" --golden --prefix-output=${fidPrefix}_${experiment_config}"
 
-    experiment=${model}_${dtype}${pruned_prefix}_${experiment_config}
+    experiment=${model}_${dtype}_${experiment_config}
 
     echo "Creating job ${experiment} with flags ${FLAGS_GOLDEN}"
 
@@ -78,14 +63,14 @@ do
 
     echo "Job submitted."
 
-    for layer in `seq 0 $nlayers`
+    for bit in `seq 0 $nbits`
     do
         for iter in `seq 1 $niters`
         do
-            experiment_config="layer_${layer}_${location}_iter_${iter}"
-            FLAGS_FAULTY=${FLAGS}" --faulty --injection --layer=${layer} --${location} --prefix-output=${fidPrefix}_${experiment_config}"
+            experiment_config="layer_${layer}_bit_${bit}_${location}_iter_${iter}"
+            FLAGS_FAULTY=${FLAGS}" --faulty --injection --layer=${layer} --bit=${bit} --${location} --prefix-output=${fidPrefix}_${experiment_config}"
 
-            experiment=${model}_${dtype}${pruned_prefix}_${experiment_config}
+            experiment=${model}_${dtype}_${experiment_config}
             
             echo "Creating job ${experiment} with flags ${FLAGS_FAULTY}"
 
