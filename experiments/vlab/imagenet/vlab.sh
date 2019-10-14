eb0c4a3514f1e95e02a28102d5fad6649f7cef6c
Brunno Goldstein <daewoo4@gmail.com>
2019-10-14 15:08:16 -0300

    Changes into experiments folder *Removed GHPCC experiment folder *Refactor on vlab.sh to handle new flags

--- experiments/vlab/imagenet/vlab.sh
+++ experiments/vlab/imagenet/vlab.sh
@@ -5,24 +5,32 @@ export PATH=$PATH:$HOME/${PROJECT}
 
 PROJECT_PATH=$HOME/$PROJECT
 DATASET_PATH=$HOME/dataset/imagenet
-JOB_PATH=${PROJECT_PATH}/experiments/vlab/job.sh
+JOB_PATH=${PROJECT_PATH}/experiments/vlab/imagenet/job.sh
 
 
 EXP_PATH=${PROJECT_PATH}/examples/imagenet
-EXP_RUN=resnet_eval.py
+EXP_RUN=imagenet_eval.py
 
 # Get command line args
-pruned=$1
+model=$1
+nlayers=$2
+pruned=$3
 
 # Set job parameters
 queue=skl
 nodes=1
-cpus=8
+cpus=16
+mem=8gb
 
 # Experiments configurations
-model=resnet50
+# model=resnet50
+# nlayers=53
+# model=resnet18
+# nlayers=20
+# model=alexnet
+# nlayers=7
+
 niters=5
-nlayers=53
 bit="random"
 location="weights"
 pruned_prefix=""
@@ -30,10 +38,10 @@ pruned_prefix=""
 FLAGS_BASE="-a ${model} -e --pretrained --batch-size=1"
 
 if [ ${pruned} -eq 1 ]; then
-    pruned_path=$2
-    prune_percent=$3
-    FLAGS_BASE+=" --pruned --pruned_file=${pruned_path}/resnet50_pruned_${prune_percent}_best.pth.tar"
-    prune_compensate=$4
+    pruned_path=$4
+    prune_percent=$5
+    FLAGS_BASE+=" --pruned --pruned_file=${pruned_path}/${model}_pruned_${prune_percent}_best.pth.tar"
+    prune_compensate=$6
     if [ ${prune_compensate} -eq 1 ]; then
         FLAGS_BASE+=" --prune_compensate"
         pruned_prefix="_pruned_${prune_percent}"
@@ -44,28 +52,23 @@ fi
 
 declare -a dtypes=("fp32" "int16" "int8")
 
-## Quantization options
-declare -a qbits=(16 8)
-declare -a qbitsAccs=(64 32)
-
 
 for dtype in "${dtypes[@]}"
 do
     FLAGS=${FLAGS_BASE}
  
     if [ ${dtype} == "int16" ]; then
-        FLAGS+=" --quantize --quant-feats=16 --quant-wts=16 --quant-accum=64"
+        FLAGS+=" --quantize --quant-mode=sym --quant-bits-acts=16 --quant-bits-wts=16 --quant-bits-accum=64"
     elif [ ${dtype} == "int8" ]; then
-        FLAGS+=" --quantize --quant-feats=8 --quant-wts=8 --quant-accum=32"
+        FLAGS+=" --quantize --quant-mode=sym --quant-bits-acts=8 --quant-bits-wts=8 --quant-bits-accum=32"
     fi
 
-    outputPath=${HOME}/experiments/iccd
+    outputPath=${HOME}/experiments/date
     outputFilePrefix=${outputPath}/${dtype}/${model}
-    fidPrefix=${outputFilePrefix}_${dtype}_norm${pruned_prefix}
     experiment_out_path=${outputPath}/${dtype}/logs
     
     experiment_config="golden"
-    FLAGS_GOLDEN=${FLAGS}" --golden --prefix-output=${fidPrefix}_${experiment_config}"
+    FLAGS_GOLDEN=${FLAGS}" --golden --record-prefix=${outputFilePrefix}_"
 
     experiment=${model}_${dtype}${pruned_prefix}_${experiment_config}
 
@@ -73,8 +76,8 @@ do
 
     echo 'Submiting job... '
 
-    echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS_GOLDEN}"
-    qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} "${FLAGS_GOLDEN}" 
+    echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus}:mem=${mem} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS_GOLDEN}"
+    qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus}:mem=${mem} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} "${FLAGS_GOLDEN}" 
 
     echo "Job submitted."
 
@@ -83,7 +86,7 @@ do
         for iter in `seq 1 $niters`
         do
             experiment_config="layer_${layer}_${location}_iter_${iter}"
-            FLAGS_FAULTY=${FLAGS}" --faulty --injection --layer=${layer} --${location} --prefix-output=${fidPrefix}_${experiment_config}"
+            FLAGS_FAULTY=${FLAGS}" --faulty --injection --layer=${layer} --${location} --iter=${iter} --record-prefix=${outputFilePrefix}_"
 
             experiment=${model}_${dtype}${pruned_prefix}_${experiment_config}
             
@@ -91,8 +94,8 @@ do
 
             echo 'Submiting job... '
 
-            echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS_FAULTY}"
-            qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} "${FLAGS_FAULTY}" 
+            echo "qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus}:mem=${mem} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} ${FLAGS_FAULTY}"
+            qsub -V -q ${queue} -lselect=${nodes}:ncpus=${cpus}:mem=${mem} -N ${experiment} -o ${experiment_out_path}/${experiment}.out -e ${experiment_out_path}/${experiment}.err -- ${JOB_PATH} ${EXP_PATH} ${EXP_RUN} ${PROJECT} ${DATASET_PATH} "${FLAGS_FAULTY}" 
 
             echo "Job submitted."
         done
